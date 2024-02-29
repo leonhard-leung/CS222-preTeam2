9807485c095ba89b565a6158f96db767f9cf72e2
Leonhard Leung <Leonhardleung@gmail.com>
2024-02-29 21:53:22 +0800

    another commit

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -20,7 +20,7 @@ public class Client extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        requestServerAddress();
+        //requestServerAddress();
 
         stage.getIcons().add(new Image(getClass().getResource("/images/client/client_app_logo.png").toExternalForm()));
         ClientModel model = new ClientModel();
